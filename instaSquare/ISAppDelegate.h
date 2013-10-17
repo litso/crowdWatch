9a8b1b9b9f4efdea4bb758ad9b6ca81dc76a9a39
Robert Manson <robmanson@gmail.com>
2013-10-16 19:25:53 -0700

    Initial Commit

---
+++ instaSquare/ISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISAppDelegate.h
+//  instaSquare
+//
+//  Created by Robert Manson on 10/16/13.
+//  Copyright (c) 2013 Robert & Sairam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ instaSquare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  instaSquare
+//
+//  Created by Robert Manson on 10/16/13.
+//  Copyright (c) 2013 Robert & Sairam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ISAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ISAppDelegate class]));
+    }
+}
